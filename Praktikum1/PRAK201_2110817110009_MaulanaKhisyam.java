54067146970e36cbf6553431cf477a094776c074
MaulanaKhisyam <103562745+MaulanaKhisyam@users.noreply.github.com>
2022-10-12 14:47:04 +0800

    Add files via upload

---
+++ Praktikum1/PRAK201_2110817110009_MaulanaKhisyam.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum1;
+import java.util.Scanner;//meambil
+
+/**
+ *
+ * @author Lenovo
+ */
+    public class PRAK201_2110817110009_MaulanaKhisyam {
+        
+    public static void main (String [] args){
+    Scanner userInput = new Scanner(System.in); 
+    int user= userInput.nextInt(); 
+    int angka = 1;
+       
+        while (angka <= 7){            
+            if (user % 2 == 0){  
+                int m;
+                m =(user / 2)-1;
+                System.out.print(" "+m+" ");               
+            }
+            else {
+                System.out.print(user);
+            }
+        user++;
+        angka++;
+        }
+    }    
+}
